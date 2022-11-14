11201d1f8670676a4dc7a726c978343e3c37572b
BetaTeamEzops <betateamezops@github.com>
2022-11-13 23:17:10 -0300

    Docker compose postgres

--- Insert_data_table.py
+++
@@ -1,8 +0,0 @@
-#  Inserindo dados na tabela “Agenda”.
-import psycopg2
-
-conn = psycopg2.connect(database="postgres, user="postgres", password="postgres4321", port="5432")
-print("Conexao com o banco de dados aberta com sucesso!")
-
-comando = conn.cursor()
-comando.execute(""" INSERT INTO AGENDA (id, Nome, Telefone))
